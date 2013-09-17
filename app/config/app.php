5ebc505c64e026ba3f3724e36f843635fa61b63d
Radoslaw Kosinski <r_kosinski@o2.pl>
2013-09-17 22:24:19 +0200

    Comment 'profiler' and 'way-generators'

--- app/config/app.php
+++ app/config/app.php
@@ -113,9 +113,9 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
-		'Way\Generators\GeneratorsServiceProvider',
+		//'Way\Generators\GeneratorsServiceProvider',
 		'Intervention\Image\ImageServiceProvider',
-		'Juy\Profiler\Providers\ProfilerServiceProvider',
+		//'Juy\Profiler\Providers\ProfilerServiceProvider',
 
 	),
 
@@ -182,7 +182,7 @@ return array(
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
 		'Image' 		  => 'Intervention\Image\Facades\Image',
-		'Profiler'        => 'Juy\Profiler\Facades\Profiler',
+		//'Profiler'        => 'Juy\Profiler\Facades\Profiler',
 
 	),
 
