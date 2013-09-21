5eaaeb756a6ecdf2ca6ea0b46fef58652162e8b0
Radoslaw Kosinski <r_kosinski@o2.pl>
2013-09-21 19:04:35 +0200

    Added menu to user setting center

--- app/views/user/settings/index.blade.php
+++ app/views/user/settings/index.blade.php
@@ -12,10 +12,14 @@
 
     <div class="row">
 
-        <div class="col-md-12">
-
-            <h1>Settings</h1>
-
+        <div class="col-md-4">
+            <div class="list-group">
+                <a href="#" class="list-group-item active">
+                    Profile
+                </a>
+                <a href="#" class="list-group-item">Account settings</a>
+                <a href="#" class="list-group-item">Notification history</a>
+            </div>
         </div>
 
     </div>
