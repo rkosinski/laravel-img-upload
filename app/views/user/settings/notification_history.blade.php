8057b86ead8fbe5f49e55c67d8e13e8a8b433544
Radoslaw Kosinski <r_kosinski@o2.pl>
2013-09-30 17:25:03 +0200

    Added view notification history;

---
+++ app/views/user/settings/notification_history.blade.php
@@ -0,0 +1,96 @@
+@extends('layouts.master')
+
+@section('main_menu')
+    @parent
+@stop
+
+@section('login')
+    @parent
+@stop
+
+@section('content')
+
+    <div class="row">
+
+        <div class="col-md-4">
+            <div class="list-group">
+                {{ HTML::linkRoute('profile_user', 'Your profile', array(), array('class' => 'list-group-item')) }}
+                {{ HTML::linkRoute('account_user', 'Account settings', array(), array('class' => 'list-group-item')) }}
+                {{ HTML::linkRoute('notification_user', 'Latest notifications', array(), array('class' => 'list-group-item')) }}
+                {{ HTML::linkRoute('notification_history', 'Notification history', array(), array('class' => 'list-group-item active')) }}
+            </div>
+        </div>
+
+        <div class="col-md-8">
+
+            <div class="panel panel-default">
+
+                <div class="panel-heading">Notification history</div>
+
+                <div class="panel-body">
+
+                    <div class="alert alert-info" id="alert">There are no notifications to show.</div>
+
+                    <div class="table-responsive">
+
+                        <table class="table table-hover">
+
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>User</th>
+                                    <th>Notification</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                {{-- */$i=1;/* --}}
+
+                                @foreach ($notifications as $notification)
+
+                                <tr>
+                                    @if ($notification->images->user_id === Auth::user()->id)
+                                        <td>{{ $i++ }}</td>
+                                        <td>{{ $notification->users->username }}</td>
+                                        <td>User voted on your image.</td>
+                                        <td>
+                                            <a href="{{ route('show_image', array('id' => $notification->images->id)) }}" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-zoom-in"></span> Show image</a>
+                                        </td>
+                                    @endif
+                                </tr>
+
+                                @endforeach
+
+                            </tbody>
+
+                        </table>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div id="iterator">{{ $i }}</div>
+
+@stop
+
+@section('add_script')
+    <script>
+    $(document).ready(function() {
+        var i = $('#iterator');
+        var alert = $('#alert');
+        i.hide();
+        alert.hide();
+        if (i.text() == 1) {
+            $('.table-responsive').hide();
+            alert.show();
+        }
+    });
+    </script>
+@stop
